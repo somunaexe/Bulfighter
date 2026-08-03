0c1fc2f93fb6a60ae9fdd7093c49660084a3e4d7
Somunachimso Nzenwa <somunanzenwa@gmail.com>
2026-08-03 02:13:28 +0100

    made the footer sticky

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="c-space py-7 mt-10 border-t border-black-300 flex justify-between items-center flex-wrap gap-3 text-sm">
+    <footer className="navbar-surface c-space bottom-0 left-0 z-20 py-7 mt-10 border-t border-black-300 flex justify-between items-center flex-wrap gap-3 text-sm">
         <div className="text-white-700 flex gap-2">
             <p>Terms & Conditions</p>
             <p>|</p>
